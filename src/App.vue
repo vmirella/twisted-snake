ce9c006a659f7731d70ee08960d840d103b8c64c
vmirella <vmirella_17@hotmail.com>
2019-05-31 17:47:43 -0500

    Creando componente para el tablero de puntajes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <div id="app">
+  <div id="app"> 
     <h1>Twisted Snake</h1>
     <h3>Score: {{score}}</h3>
     <div class="div-button">
       <button v-on:click="isPlaying ? stop() : start()">{{isPlaying ? "Stop" : "Play"}}</button>
     </div>
-    <SnakeCanvas
-      :cellSize="cellSize"
-      :countCells="countCells"
-      :speed="speed"
-      :isPlaying="isPlaying"
-      :score="score"
-      :stop="stop"
-      :addScore="addScore"
-      :points="points"
-    >
-    </SnakeCanvas>
+
+    <div class="score">
+      <ScoresBoard
+        :scores="scores"
+      >
+      </ScoresBoard>
+    </div>
+    <div class="board">
+      
+      <SnakeCanvas
+        :cellSize="cellSize"
+        :countCells="countCells"
+        :speed="speed"
+        :isPlaying="isPlaying"
+        :score="score"
+        :stop="stop"
+        :addScore="addScore"
+        :points="points"
+      >
+      </SnakeCanvas>
+    </div>
+    
     
     
   </div>
@@ -23,11 +34,13 @@
 
 <script>
 import SnakeCanvas from './components/SnakeCanvas.vue'
+import ScoresBoard from './components/Scores.vue'
 
 export default {
   name: 'app',
   components: {
-    SnakeCanvas
+    SnakeCanvas,
+    ScoresBoard
   },
   data () {
     return {
@@ -36,7 +49,8 @@ export default {
       speed: 5,
       isPlaying: false,
       score: 0,
-      points: 1
+      points: 1,
+      scores: []
     }
   },
   methods: {
@@ -46,6 +60,7 @@ export default {
     },
     stop () {
       this.isPlaying = false
+      this.scores.push(this.score)
     },
     addScore (score) {
       this.score += score
@@ -61,11 +76,28 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    flex: wrap;
-    justify-content: center;
-    align-items: center;
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+  .score, .board {
+    float: left;
+  }
+  .score {
+    width: 20%;
+    text-align: left;
+  }
+  .board {
+    width: 80%;
   }
   .div-button {
+    margin-bottom: 20px;
+  }
 
+  button {
+    font-size: 30px;
+    cursor: pointer;
+    padding: 5px 20px;
+    border-radius: 5px;
   }
 </style>
---
+++ src/components/Scores.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Scores:</h2>
+    <ul>
+      <li v-for="(score, index) in orderedScores" v-bind:key="index">Score {{index + 1}}: {{score}} points</li>
+    </ul>
+  </div>  
+</template>
+
+<script>
+export default {
+  name:'Scores',
+  props: {
+    scores: Array
+  },
+  computed: {
+    orderedScores: function () {
+      return this.scores.sort((a, b) => {return b - a})
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    padding-left: 20px;
+  }
+</style>
+
+
